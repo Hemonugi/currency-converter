6ff11c42f0fae1bd01b971c7e3654251dc025413
hemonugi <hemonugi@gmail.com>
2022-11-22 23:35:06 +0400

    added input numbers

--- app/src/main/java/com/hemonugi/currency_converter/MainActivity.kt
+++ app/src/main/java/com/hemonugi/currency_converter/MainActivity.kt
@@ -7,10 +7,33 @@ import com.hemonugi.currency_converter.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var expressionString = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setListeners(binding)
+    }
+
+    private fun setListeners(binding: ActivityMainBinding) {
+        val buttons = listOf(binding.numZero,
+            binding.numOne,
+            binding.numTwo,
+            binding.numThree,
+            binding.numFour,
+            binding.numFive,
+            binding.numSix,
+            binding.numSeven,
+            binding.numEight,
+            binding.numNine,
+        )
+
+        buttons.forEach { button -> button.setOnClickListener {
+                expressionString += button.text
+                binding.expInput.text = expressionString
+            }
+        }
     }
 }
