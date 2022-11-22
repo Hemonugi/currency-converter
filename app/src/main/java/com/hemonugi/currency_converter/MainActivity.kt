e77de269b79457a3633a8fb25bfd744b17add753
hemonugi <hemonugi@gmail.com>
2022-11-22 22:45:31 +0400

    migrate to binding

--- app/src/main/java/com/hemonugi/currency_converter/MainActivity.kt
+++ app/src/main/java/com/hemonugi/currency_converter/MainActivity.kt
@@ -2,10 +2,15 @@ package com.hemonugi.currency_converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hemonugi.currency_converter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
